9879c817d29cbd2affd3ad46f942e4b71f1806cb
Guilherme Schroder <gvschroder@gmail.com>
2025-02-15 05:36:19 -0300

    [Add] Yamls, HTMLs Metadata

--- scripts/yaml2html.py
+++ scripts/yaml2html.py
@@ -3,24 +3,40 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 YAML_DIR = '../yaml'
-OUTPUT_DIR = '../site/views/metadados'
+OUTPUT_DIR = '../portal/views/metadados'
+
+def list_yaml(path):
+    yaml_files = []
+    for root_path, _, filles in os.walk(path):
+        for filename in filles:
+            if filename.endswith(('.yaml', '.yml')):
+                yaml_files.append({
+                    "path": os.path.join(root_path, filename),
+                    "filename": filename
+                })
+    return yaml_files
 
 def main():
-    yaml_files = os.listdir(YAML_DIR)
-    for filename in yaml_files:
-        with open(f'{YAML_DIR}/{filename}', 'r') as f:
-            yaml_content = yaml.safe_load(f)
+    for el in list_yaml(YAML_DIR):
+        filename = el['filename']
+        path = el['path']
+        try:
+            with open(f'{path}', 'r') as f:
+                yaml_content = yaml.safe_load(f)
 
-        env = Environment(loader=FileSystemLoader('.'))
-        template = env.get_template('templates/metadata.html')
+            env = Environment(loader=FileSystemLoader('.'))
+            template = env.get_template('templates/metadata.html')
 
-        output = template.render(yaml_content)
-        
-        output_path = f"{OUTPUT_DIR}/{yaml_content['catalog']}/{yaml_content['schema']}"
-        os.makedirs(output_path, exist_ok=True)
+            output = template.render(yaml_content)
+            
+            output_path = f"{OUTPUT_DIR}/{yaml_content['catalog']}/{yaml_content['schema']}"
+            os.makedirs(output_path, exist_ok=True)
 
-        with open(f'{output_path}/{filename.split(".")[0]}.html', 'w') as file:
-            file.write(output)
+            with open(f'{output_path}/{filename.split(".")[0]}.html', 'w') as file:
+                file.write(output)
+        except Exception as ex:
+            print(f"File: {filename}")
+            print(f"Error: {ex}")
 
 if __name__ == "__main__":
     main()
